619f4327414c95532e7cfc1ea6fa3320390536fd
René Mundt <renem@live.dk>
2017-09-01 07:01:12 +0200

    feat(mLab): uncomment code and set conenctionstring

--- mongo/expense-handler.js
+++ mongo/expense-handler.js
@@ -1,21 +1,24 @@
 module.exports = (router) => {
 
     var bodyParser = require('body-parser')
-    // var mongoose = require('mongoose')
+    var mongoose = require('mongoose')
     var Expense = require('./models/expense')
-    // var ModelMapper = require('./model-mapper')
+    var ModelMapper = require('./model-mapper')
 
-    // mongoose.Promise = require('bluebird')
+    mongoose.Promise = require('bluebird')
 
+    mongoose.connect('mongodb://em_user:pwd4Mlab@ds119524.mlab.com:19524/expense-manager')
     // mongoose.connect('mongodb://localhost/expense-manager')
 
-    // var db = mongoose.connection
+    var db = mongoose.connection
 
-    // db.on('error', console.error.bind(console, 'connection error:'))
-    // db.once('open', () => {
-        // // we are connected
-    // })
-
+    db.on('error', console.error.bind(console, 'connection error:'))
+    db.once('open', () => {
+        // we are connected
+    })
+// user     : ""em_user
+// password : "+>GqX3r,H3zQex>p"
+// mongodb://<dbuser>:<dbpassword>@ds119524.mlab.com:19524/expense-manager
     router.use((req, res, next) => {
         console.log('route: ', req.originalUrl)
         next()
@@ -24,63 +27,62 @@ module.exports = (router) => {
     router.route('/expenses')
 
         .get((req, res) => {
-            res.json('IT WORKS!!')
-            // Expense.find((err, expenses) => {
-            //     if (err)
-            //         res.send(err)
-            //     res.json(ModelMapper.MapExpenses(expenses))
-            // })
+            Expense.find((err, expenses) => {
+                if (err)
+                    res.send(err)
+                res.json(ModelMapper.mapExpenses(expenses))
+            })
         })
 
-    //     .post((req, res) => {
-    //         var expense = new Expense()
-    //         expense.created = new Date()
-    //         expense.amount = req.body.amount
-    //         expense.store = req.body.store
-    //         expense.profile.id = 'xxxxxx'
-    //         expense.profile.givenName = 'René Mundt'
+        .post((req, res) => {
+            var expense = new Expense()
+            expense.created = new Date()
+            expense.amount = req.body.amount
+            expense.store = req.body.store
+            expense.profile.id = 'xxxxxx'
+            expense.profile.givenName = 'René Mundt'
 
-    //         expense.save((err) => {
-    //             if (err)
-    //                 res.send(err)
-    //             res.json({ message: 'Expense created' })
-    //         })
-    //     })
+            expense.save((err) => {
+                if (err)
+                    res.send(err)
+                res.json({ message: 'Expense created' })
+            })
+        })
 
-    // router.route('/expenses/:expense_id')
+    router.route('/expenses/:expense_id')
 
-    //     .get((req, res) => {
-    //         Expense.findById(req.params.expense_id, (err, expense) => {
-    //             if (err)
-    //                 res.send(err)
-    //             res.json(ModelMapper.MapExpense(expense))
-    //         })
-    //     })
+        .get((req, res) => {
+            Expense.findById(req.params.expense_id, (err, expense) => {
+                if (err)
+                    res.send(err)
+                res.json(ModelMapper.MapExpense(expense))
+            })
+        })
 
-    //     .put((req, res) => {
-    //         Expense.findById(req.params.expense_id, (err, expense) => {
-    //             if (err)
-    //                 res.send(err)
-    //             expense.updated = new Date()
-    //             expense.amount = req.body.amount
-    //             expense.store = req.body.store
+        .put((req, res) => {
+            Expense.findById(req.params.expense_id, (err, expense) => {
+                if (err)
+                    res.send(err)
+                expense.updated = new Date()
+                expense.amount = req.body.amount
+                expense.store = req.body.store
 
-    //             expense.save((err) => {
-    //                 if (err)
-    //                     res.send(err)
-    //                 res.json({ message: 'Expense updated' })
-    //             })
-    //         })
-    //     })
+                expense.save((err) => {
+                    if (err)
+                        res.send(err)
+                    res.json({ message: 'Expense updated' })
+                })
+            })
+        })
 
-    //     .delete((req, res) => {
-    //         Expense.remove({
-    //             _id: req.params.expense_id
-    //         }, (err) => {
-    //             if (err)
-    //                 res.send(err)
-    //             res.json('Expense deleted')
-    //         })
-    //     })
+        .delete((req, res) => {
+            Expense.remove({
+                _id: req.params.expense_id
+            }, (err) => {
+                if (err)
+                    res.send(err)
+                res.json('Expense deleted')
+            })
+        })
 
 }
